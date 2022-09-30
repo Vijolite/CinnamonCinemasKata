c4700d1fa83a1eecb217923b7260490b3ca3ef65
Ija Saporenkova <ija.sha@gmail.com>
2022-09-30 18:18:42 +0100

    Add UserInterface and random seat generation

--- CinemasService/Program.cs
+++ CinemasService/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace CinemasService
+﻿using CinemasCervice;
+
+namespace CinemasService
 {
     public class Program
     {
         public static void Main()
         {
             var cinema = new CinemaHall();
+            int numberOfSeats = UserInterface.GenerateRandomNumberOfSeats();
+            while (cinema.SeatsCouldBeAllocated(numberOfSeats))
+            {
+                cinema.AllocateSeats(numberOfSeats);
+            }
             
         }
     }
