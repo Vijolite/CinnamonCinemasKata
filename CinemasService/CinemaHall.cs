64946d90e69c371654e470fbc6ca954d49954b44
Ija Saporenkova <ija.sha@gmail.com>
2022-10-01 09:55:55 +0100

    Delete unnecessary using...

--- CinemasService/CinemaHall.cs
+++ CinemasService/CinemaHall.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+
 
 
 namespace CinemasService
